c120535945bc3044d6c22fcd85077002734fde20
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-07-07 08:03:21 -0700

    js-asignment-10

---
+++ script.js
@@ -0,0 +1,103 @@
+
+
+
+
+// smallest number
+
+// var numbers = [];
+
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("enter the number")
+  
+//     var number = numbers.push(userInput);
+// }
+
+// var smallestNumber = numbers[0];
+
+// for (var i = 1; i < numbers.length; i++) {
+//     if (numbers[i] < smallestNumber) {
+//         smallestNumber = numbers[i];
+//     }
+// }
+
+// console.log("The smallest number in the array is:", smallestNumber);
+
+
+
+
+
+// celcius to fahrenheit
+
+var n = +prompt("please enter a temperature in farenhiet")
+
+var temperatureInFahrenheit = 0;
+
+var num = (n * 9.0 / 5.0) + 32.0;
+
+temperatureInFahrenheit += num;
+console.log("temperatureInFahrenheit : " + temperatureInFahrenheit)
+
+
+
+
+
+
+
+// fahrenheit to celcius
+
+
+// var n = +prompt("please enter tempreture in fahrenheit")
+
+// var temperatureInCelcius = 0;
+
+// var num = (n  -  32)  * 5/9;
+
+// temperatureInCelcius += num;
+
+// console.log("Tempreature in celcius : " + temperatureInCelcius)
+
+
+
+
+
+
+// reverse an array
+
+
+// var arr = [];
+
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("enter the number")
+  
+//     var numbers = arr.push(userInput);
+// }
+
+// var result = [];
+// for (var i = arr.length - 1; i >= 0; i--) {
+//     result += arr[i];
+// }
+// console.log(result);
+
+
+
+
+
+
+// average of an array
+
+// var arr = [];
+
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("enter the number")
+  
+//     var numbers = arr.push(userInput);
+// }
+
+// var sum = 0;
+// for(var i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// const average = sum / arr.length;
+// console.log(average)
+
+
